b9b839e407654bc0cf22d94b355441cf80b34407
Dhruti Shah <dhrutishah10@gmail.com>
2015-03-09 18:14:43 +0300

    changed production

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,7 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
    def thanks_mail_preview
-    UserMailer.thanks_email(Registration.last)
+    UserMailer.thanks_email(Registration.first)
   end
 
 end
