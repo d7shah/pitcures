7fd609eecad6406e2e9a82df930d39745e995ca9
dwanjuhi1 <dwanjuhi@alueducation.com>
2015-03-10 14:17:09 +0000

    fixed city controller

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -69,6 +69,7 @@ class CitiesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def city_params
-      params[:city]
+      params.require(:city).permit(:location, :citim)
+      
     end
 end
