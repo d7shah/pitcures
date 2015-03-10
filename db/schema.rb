ce1bb2dd5efa337074b16661c7f5aac852119f76
dwanjuhi1 <dwanjuhi@alueducation.com>
2015-03-10 04:32:28 +0000

    added navbar to registrations page

--- db/schema.rb
+++ db/schema.rb
@@ -28,10 +28,4 @@ ActiveRecord::Schema.define(version: 20150306051710) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
